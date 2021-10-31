9305facc0f9a15f9e61a41ac353e00d815092825
B207 <91238174+csjh240@users.noreply.github.com>
2021-10-31 20:55:14 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,27 @@
-
+pins.onPulsed(DigitalPin.P0, PulseValue.High, function () {
+    basic.showLeds(`
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+pins.onPulsed(DigitalPin.P0, PulseValue.Low, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        `)
+})
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.forever(function () {
+	
+})
